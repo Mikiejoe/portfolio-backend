5858981dffbf7b42eb780ac644c92fe02036d249
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-05-20 15:13:07 +0300

    cors config

--- cms/settings.py
+++ cms/settings.py
@@ -51,6 +51,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -61,8 +62,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'cms.urls'
 
-# CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
-CORS_ALLLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
+# CORS_ALLLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
