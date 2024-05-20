bf4b73d7a49e5e1adf83d97cf2bba102c40fbb2c
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-05-20 13:19:37 +0300

    collect static

--- cms/urls.py
+++ cms/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
