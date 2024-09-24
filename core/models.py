6edc2b806e5256d0ed2e5886fd847a938002d14c
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-09-24 16:28:55 +0300

    update the project_name in serializers

--- core/models.py
+++ core/models.py
@@ -16,29 +16,15 @@ class Project(models.Model):
     live_url = models.URLField(null=True)
 
     def __str__(self):
-        return self.title
-    # title = models.CharField(max_length=100)
-    # description = models.TextField()
-    # image = models.ImageField(upload_to=f'portfolio/')
-    # url = models.URLField(blank=True)
-
-    # def __str__(self):
-    #     return self.title
-    
-    
-    
+        return self.title  
+       
 class Photo(models.Model):
-    # project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    # image = models.ImageField(upload_to=f'portfolio/images/')
-
-    # def __str__(self):
-    #     return self.project.title
     project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='project_images/')
+    image = models.ImageField(upload_to=f'portfolio/project-images/{project.title}')
     uploaded_at = models.DateTimeField(auto_now_add=True,null=True)
 
     def __str__(self):
-        return f"Image for {self.project.project_name}"
+        return f"Image for {self.project.name}"
     
     
 class Emails(models.Model):
--- core/serializers.py
+++ core/serializers.py
@@ -12,7 +12,7 @@ class ProjectSerializer(ModelSerializer):
     images = PhotoSerializer(many=True, read_only=True)
     class Meta:
         model = Project
-        fields = ['project_name', 'technology', 'overview', 'key_features', 'development', 'conclusion', 'images']
+        fields = ['title', 'technology', 'overview', 'key_features', 'development', 'conclusion', 'images']
                  
 
 class EmailsSerializer(ModelSerializer):
