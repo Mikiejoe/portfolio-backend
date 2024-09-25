a0a373a183841689f2e6d604c5a4040716dd064a
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-09-25 14:22:01 +0300

    update project detail url to use title insteadof pk

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import ProjectListCreateAPIView, ProjectRetrieveUpdateDestroyAPIView, PhotoListCreateAPIView,send_email,send_email_with_template,ProjectViewSet
+from .views import send_email_with_template,ProjectViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -7,10 +7,7 @@ router.register(r'projects', ProjectViewSet)
 
 
 urlpatterns = [
-    path('projects/', ProjectListCreateAPIView.as_view(), name='projects'),
-    path('projects/<int:pk>/', ProjectRetrieveUpdateDestroyAPIView.as_view(), name='project'),
-    path('photos/', PhotoListCreateAPIView.as_view(), name='photos'),
-    # path('send-email/', send_email, name='send_email'),
+    # path('photos/', PhotoListCreateAPIView.as_view(), name='photos'),
     path('send-email/', send_email_with_template, name='send_email'),
     path('', include(router.urls)),
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,11 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.core.mail import send_mail
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status,viewsets
-from .models import Project, Photo
-from .serializers import ProjectSerializer, PhotoSerializer,EmailsSerializer
+from .models import Project
+from .serializers import ProjectSerializer, EmailsSerializer
 from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
 
@@ -15,49 +14,13 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
-class ProjectListCreateAPIView(ListCreateAPIView):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
+    def retrieve(self, request,pk=None, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = get_object_or_404(queryset, title=pk)
+        self.check_object_permissions(self.request, obj)
+        return Response(ProjectSerializer(obj).data)
+
     
-class ProjectRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    
-class PhotoListCreateAPIView(ListCreateAPIView):
-    queryset = Photo.objects.all()
-    serializer_class = PhotoSerializer
-    
-    
-@api_view(["POST"])
-def send_email(request):    
-    if request.method == 'POST':
-        # Extract data from the request and send the data to my email
-        try:
-            print(request.data)                
-            subject = request.data['subject'] + " from " + request.data['name']
-            message = request.data['message']
-            name = request.data['name']
-            recepient_list = request.data['email']
-            print(message)
-            send_mail(subject=subject, message=message,from_email=settings.EMAIL_HOST_USER, recipient_list=[settings.EMAIL_HOST_USER])
-        
-            
-            # Send a confirmation email to the user
-            subject = "Joseph Omondi Received a message from you."
-            name = request.data['name']
-            message = f"Dear {name},\nThank you for reaching out to me through my portfolio, I will get back to you as soon as possible."
-            print(message)
-            res = send_mail(subject, message,from_email=settings.EMAIL_HOST_USER, recipient_list=[recepient_list])
-            print("result is ",res)
-            serializer = EmailsSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(status=status.HTTP_200_OK,data={"message":"Message sent successfully!!"})
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            print(e)
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR,data={"message":"An error occurred"})
-    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["POST"])
 def send_email_with_template(request):
