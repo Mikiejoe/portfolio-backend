4c186cd5979db212a4cd32d040d1cd80500164c5
Michael Joseph <99811467+Mikiejoe@users.noreply.github.com>
2024-10-04 12:09:41 +0300

    Update settings.py

--- cms/settings.py
+++ cms/settings.py
@@ -105,7 +105,6 @@ if "test" in sys.argv:
         "NAME": ":memory:",
     }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -142,6 +141,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
